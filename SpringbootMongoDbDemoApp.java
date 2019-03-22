4ba7063eca35efc31bb68e29f0f39dbca30f3f6b
Siddharth Singh <19413466+Siddharthpratapsingh@users.noreply.github.com>
2019-03-22 16:21:32 +0530

    Add files via upload

---
+++ SpringbootMongoDbDemoApp.java
@@ -0,0 +1,12 @@
+package com.truck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoDbDemoApp{
+	public static void main(String[] args)
+	{
+	SpringApplication.run(SpringbootMongoDbDemoApp.class, args);
+}
+}
